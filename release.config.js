3fc93a2820c3f0372ac0d1562c74d619061fc0de
Tanner B. Hess Webber <tb@hesswebber.com>
2024-01-13 01:04:19 -0500

    ci(release): removes git plugin from semantic release

--- release.config.js
+++ release.config.js
@@ -41,7 +41,6 @@ const config = {
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
     "@semantic-release/npm",
-    "@semantic-release/git",
     "@semantic-release/github",
   ],
 };
